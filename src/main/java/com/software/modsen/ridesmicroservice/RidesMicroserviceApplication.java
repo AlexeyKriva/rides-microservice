896f294769d270ab671a9bb55f76e36939e7408b
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-09-20 03:30:32 +0300

    chore: init rides microservice

---
+++ src/main/java/com/software/modsen/ridesmicroservice/RidesMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.software.modsen.ridesmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidesMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RidesMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/software/modsen/ridesmicroservice/RidesMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.modsen.ridesmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidesMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
