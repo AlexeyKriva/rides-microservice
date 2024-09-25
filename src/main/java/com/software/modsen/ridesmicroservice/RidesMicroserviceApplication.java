c77e65260c9943442fad905a8272d29256da0057
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-09-25 17:41:55 +0300

    chore: init eureka client

--- src/main/java/com/software/modsen/ridesmicroservice/RidesMicroserviceApplication.java
+++ src/main/java/com/software/modsen/ridesmicroservice/RidesMicroserviceApplication.java
@@ -2,12 +2,14 @@ package com.software.modsen.ridesmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
 @EnableFeignClients
+@EnableDiscoveryClient
 public class RidesMicroserviceApplication {
 
     public static void main(String[] args) {
